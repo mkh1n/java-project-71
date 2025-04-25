da3c757c4138bb28bdd460fff1e943736aba14f4
roma <romamuhino1@gmail.com>
2025-04-25 06:28:09 +0300

    step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -8,10 +9,13 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+application{
+    mainClass = "hexlet.code.App"
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.7")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,20 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
-public class App {
+import java.util.concurrent.Callable;
+
+@Command(mixinStandardHelpOptions = true, name = "gendiff", version = "gendiff - 1.0.0", description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 
-        System.out.printf("Hello and welcome!");
+    @Override
+    public Integer call() throws Exception {
+        // Ваш код здесь
+        System.out.println("Hello and welcome!");
+        return 0;
     }
 }
